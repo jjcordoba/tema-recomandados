17b8bb08c64b08f1d0cb327afdaacdebfa024678
MULTISERVICIOS ARI <multiservicios.ari@hotmail.com>
2023-06-17 02:02:20 -0500

    1

--- functions.php
+++ functions.php
@@ -7,3 +7,13 @@ require_once 'personal/departamentos.php';
 require_once 'personal/slider-categorias.php';
 require_once 'personal/mostrarvariaciones.php';
 require_once 'personal/botones.php';
+
+
+add_action('wp_head', 'dcms_show_template_file_name');
+function dcms_show_template_file_name() {
+    global $template;
+    	$html = "<div style='background:#23282d;padding:4px 10px;color:#eee;font-size:13px;'> ➜ ";
+    	$html .= $template;
+    	$html .= "</div>";
+    echo $html;
+}
