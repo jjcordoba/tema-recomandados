d095e043e0070b3c7957056adc023ea5edaa8cd5
MULTISERVICIOS ARI <multiservicios.ari@hotmail.com>
2023-06-21 23:50:22 -0500

    productos falta link deseos add card

--- functions.php
+++ functions.php
@@ -8,13 +8,14 @@ require_once 'personal/slider-categorias.php';
 require_once 'personal/mostrarvariaciones.php';
 require_once 'personal/botones.php';
 require_once 'personal/productos-oferta.php';
+require_once 'personal/productos.php';
 
 
 add_action('wp_head', 'dcms_show_template_file_name');
 function dcms_show_template_file_name() {
-    global $template;
-    	$html = "<div style='background:#23282d;padding:4px 10px;color:#eee;font-size:13px;'> ➜ ";
-    	$html .= $template;
-    	$html .= "</div>";
+    $template = get_page_template();
+    $html = "<div style='background:#23282d;padding:4px 10px;color:#eee;font-size:13px;'> ➜ ";
+    $html .= $template;
+    $html .= "</div>";
     echo $html;
 }
--- personal/productos.php
+++ personal/productos.php
@@ -1,5 +1,5 @@
 <?php 
-function mostrar_productos_al_azar()
+function mostrar_todos_los_productos()
 {
     $args = array(
         'post_type' => 'product',
@@ -9,7 +9,6 @@ function mostrar_productos_al_azar()
     $productos = new WP_Query($args);
     if ($productos->have_posts()) {
         echo '<div class="productos-container">';
-        $count = 0;
         while ($productos->have_posts()) {
             $productos->the_post();
             global $product;
@@ -19,14 +18,10 @@ function mostrar_productos_al_azar()
             echo '<p>Categoría: ' . get_the_terms(get_the_ID(), 'product_cat')[0]->name . '</p>';
             echo '<h3>' . get_the_title() . '</h3>';
             echo '<p>Precio: ' . $product->get_price_html() . '</p>';
-            echo '<button class="agregar_al_carrito" data-product-id="' . get_the_ID() . '">Agregar al carrito</button>';
+            echo '<button class="agregar-carrito" data-product-id="' . get_the_ID() . '">Agregar al carrito</button>';
             echo '<p class="deseos" data-product-id="' . get_the_ID() . '">Agregar a la lista de deseos</p>';
             echo '</div>';
             echo '</div>';
-            $count++;
-            if ($count >= 4) { // Máximo 4 productos por fila
-                break;
-            }
         }
         echo '</div>';
     }
@@ -39,11 +34,9 @@ function mostrar_productos_al_azar_css()
         .productos-container {
             display: flex;
             flex-wrap: wrap;
-            justify-content: flex-start;
+            justify-content: space-between;
         }
         .producto {
-            display: flex;
-            align-items: center;
             width: 23%;
             margin-bottom: 20px;
             background-color: #fff;
@@ -51,18 +44,20 @@ function mostrar_productos_al_azar_css()
             border-radius: 5px;
             padding: 10px;
             box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
-          }
-          
-          .producto img {
-            width: 30%;
+        }
+        .producto img {
+            width: 100%;
             height: auto;
+            display: block;
+            margin-bottom: 10px;
+            float: left;
             margin-right: 10px;
-          }
-          
-          .producto .contenido {
-            width: 50%;
-          }
-          
+        }
+        .producto .contenido {
+            margin-left: 10px;
+            float: left;
+            width: 75%;
+        }
         .producto p,
         .producto h3 {
             margin-left: 0;
@@ -129,11 +124,10 @@ function mostrar_productos_al_azar_css()
 function agregar_al_carrito_ajax()
 {
     wp_enqueue_script('jquery');
-    $ajax_nonce = wp_create_nonce('agregar-al-carrito-nonce');
     ?>
     <script>
         jQuery(document).ready(function($) {
-            $(document).on('click', '.agregar_al_carrito', function(e) {
+            $(document).on('click', '.agregar-carrito', function(e) {
                 e.preventDefault();
                 var productId = $(this).data('product-id');
                 $.ajax({
@@ -141,15 +135,15 @@ function agregar_al_carrito_ajax()
                     url: '<?php echo admin_url('admin-ajax.php'); ?>',
                     data: {
                         action: 'agregar_al_carrito',
-                        product_id: productId,
-                        security: '<?php echo $ajax_nonce; ?>'
+                        product_id: productId
                     },
                     success: function(response) {
                         alert('El producto ha sido agregado al carrito.');
                     }
                 });
             });
-             $(document).on('click', '.deseos', function(e) {
+
+            $(document).on('click', '.deseos', function(e) {
                 e.preventDefault();
                 var productId = $(this).data('product-id');
                 $.ajax({
@@ -157,8 +151,7 @@ function agregar_al_carrito_ajax()
                     url: '<?php echo admin_url('admin-ajax.php'); ?>',
                     data: {
                         action: 'agregar_a_lista_deseos',
-                        product_id: productId,
-                        security: '<?php echo $ajax_nonce; ?>'
+                        product_id: productId
                     },
                     success: function(response) {
                         alert('El producto ha sido agregado a la lista de deseos.');
@@ -169,24 +162,23 @@ function agregar_al_carrito_ajax()
     </script>
     <?php
 }
- function agregar_al_carrito()
+
+function agregar_al_carrito()
 {
-    check_ajax_referer('agregar-al-carrito-nonce', 'security');
     if (isset($_POST['product_id'])) {
         $product_id = $_POST['product_id'];
         WC()->cart->add_to_cart($product_id);
     }
     wp_die();
 }
- function agregar_a_lista_deseos()
+
+function agregar_a_lista_deseos()
 {
-    check_ajax_referer('agregar-al-carrito-nonce', 'security');
     if (isset($_POST['product_id'])) {
         $product_id = $_POST['product_id'];
         // Agregar el código para agregar a la lista de deseos aquí
     }
     wp_die();
 }
-  
-add_shortcode('todos_productos', 'mostrar_productos_al_azar');
-add_action('wp_head', 'mostrar_productos_al_azar_css');
+
+add_shortcode('todos_los_productos', 'mostrar_todos_los_productos');
